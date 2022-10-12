9a79909e1ea5b1b1773676c3408802ba55b375ec
HyagoFellipee <hyago.soares@cear.ufpb.br>
2022-10-12 13:32:43 -0300

    adiciona a primeira versão da funcionalidade de Login e Cadastro de Usuários

--- petoteca/login/views.py
+++ petoteca/login/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render, redirect 
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth.decorators import login_required 
+from django.contrib import messages
 
-# Create your views here.
+
+from .forms import CreateUserForm 
+
+@login_required(login_url='login:cadastro')
+def loggedPage(request): 
+    context = {}
+    return render(request, 'login\login.html', context)
+
+def logoutUser(request): 
+    logout(request)
+    return redirect('login:cadastro')
+
+def cadastroPage(request): 
+    #TODO 
+    # Criar sequência de mensagens apontando erros durante o login
+    if not request.user.is_authenticated: 
+        form = CreateUserForm();  
+
+        if request.method == 'POST':
+
+            if request.POST.get('realSignUp'): 
+                form = CreateUserForm(request.POST)
+                #form.clean_email()
+                if form.is_valid(): 
+                    form.save()
+                    messages.success(request, 'Clique no link enviado ao email cadastrado e verifique-o.')
+                else: 
+                    messages.error(request, 'Erro na validação dos seus dados pessoais')
+            elif request.POST.get('realSignIn'):
+                username = request.POST.get('username')      
+                password = request.POST.get('password')      
+                user = authenticate(request, username=username, password=password)
+
+                if user is not None: 
+                    login(request, user)
+                    return redirect('login:login_home')
+                else:
+                    messages.info(request, 'Senha ou usuário incorretos')
+    else: return redirect('login:login_home')
+
+    context = {'form': form}
+    return render(request, 'login\cadastro.html', context)
