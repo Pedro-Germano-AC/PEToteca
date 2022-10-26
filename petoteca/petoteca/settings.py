00bb8d056121ed05d6b5cb71c0cd6a101d879155
HyagoFellipee <hyago.soares@cear.ufpb.br>
2022-10-26 14:36:52 -0300

    adiciona a funcionalidade de confirmação de email

--- petoteca/petoteca/settings.py
+++ petoteca/petoteca/settings.py
@@ -134,4 +134,13 @@ MESSAGE_TAGS = {
         messages.SUCCESS: 'alert-success',
         messages.WARNING: 'alert-warning',
         messages.ERROR: 'alert-danger',
- }
+}
+
+#Email 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_FROM = 'petotecaoficial@gmail.com'
+EMAIL_HOST_USER = 'petotecaoficial@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
